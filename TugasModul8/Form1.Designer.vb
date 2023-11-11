93ee1508f0a18304c55a1e41289dcfbd73cca41f
Razmi <razmi675@gmail.com>
2023-11-12 01:08:25 +0700

    projek GUI

---
+++ TugasModul8/Form1.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        ComboBox1 = New ComboBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        TextBox1 = New TextBox()
+        TextBox4 = New TextBox()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(338, 244)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 1
+        Button1.Text = "Konversi"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(272, 125)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(217, 23)
+        ComboBox1.TabIndex = 13
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(211, 204)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 14
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(434, 204)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 15
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(211, 177)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(41, 15)
+        Label1.TabIndex = 16
+        Label1.Text = "Label1"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(434, 177)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(41, 15)
+        Label2.TabIndex = 17
+        Label2.Text = "Label2"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(365, 88)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(34, 15)
+        Label3.TabIndex = 18
+        Label3.Text = "Suhu"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(434, 252)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(41, 15)
+        Label5.TabIndex = 25
+        Label5.Text = "Label5"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(211, 252)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(41, 15)
+        Label6.TabIndex = 24
+        Label6.Text = "Label6"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(434, 279)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 23
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(211, 279)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(100, 23)
+        TextBox4.TabIndex = 22
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(128, 208)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(58, 19)
+        RadioButton1.TabIndex = 26
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Active"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(128, 283)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(58, 19)
+        RadioButton2.TabIndex = 27
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Active"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(RadioButton2)
+        Controls.Add(RadioButton1)
+        Controls.Add(Label5)
+        Controls.Add(Label6)
+        Controls.Add(TextBox1)
+        Controls.Add(TextBox4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(ComboBox1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+End Class
---
+++ TugasModul8/Form1.vb
@@ -0,0 +1,146 @@
+﻿Imports TempConv
+
+Public Class Form1
+    Private currentIndex As Integer = 0
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        ' Get the selected item from the ComboBox
+        Dim selectedItem As String = ComboBox1.SelectedItem.ToString()
+
+        ' Split the selected item into words
+        Dim words As String() = selectedItem.Split(" "c)
+
+        ' Display the second word in Label1
+        If words.Length >= 2 Then
+            Label1.Text = words(1)
+            Label5.Text = words(1)
+        Else
+            Label1.Text = "N/A"
+        End If
+
+        ' Display the first word in Label2
+        If words.Length >= 1 Then
+            Label2.Text = words(3)
+            Label6.Text = words(3)
+        Else
+            Label2.Text = "N/A"
+        End If
+
+        For Each ctrl As Control In Me.Controls
+            If TypeOf ctrl Is TextBox Then
+                DirectCast(ctrl, TextBox).Text = String.Empty
+            End If
+        Next
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "Temperature Conversion Calculator"
+        RadioButton1.Checked = True
+        TextBox3.ReadOnly = True
+        TextBox3.Enabled = False
+        TextBox1.ReadOnly = True
+        TextBox1.Enabled = False
+        ComboBox1.Items.AddRange({
+            "Konversi Celcius - Reamur",
+            "Konversi Celcius - Fahrenheit",
+            "Konversi Celcius - Kelvin",
+            "Konversi Reamur - Fahrenheit",
+            "Konversi Reamur - Kelvin",
+            "Konversi Fahrenheit - Kelvin"
+        })
+
+        ComboBox1.SelectedIndex = 0
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim selectedItem As String = ComboBox1.SelectedItem.ToString()
+        Dim objConv As New TempConv.TempConvComp
+        ' Convert the input value based on the selected item
+        Dim inputValue As Double
+        Dim result As Double
+        If Double.TryParse(TextBox2.Text, inputValue) Then
+            TextBox1.Text = inputValue
+            If TextBox2.Enabled AndAlso Not TextBox2.ReadOnly Then
+                Select Case selectedItem
+                    Case "Konversi Celcius - Reamur"
+                        TextBox4.Text = objConv.cTor(inputValue).ToString()
+                        TextBox3.Text = objConv.cTor(inputValue).ToString()
+                    Case "Konversi Celcius - Fahrenheit"
+                        TextBox4.Text = objConv.cToF(inputValue).ToString()
+                        TextBox3.Text = objConv.cToF(inputValue).ToString()
+                    Case "Konversi Celcius - Kelvin"
+                        TextBox4.Text = objConv.cTok(inputValue).ToString()
+                        TextBox3.Text = objConv.cTok(inputValue).ToString()
+
+                    Case "Konversi Reamur - Fahrenheit"
+                        TextBox4.Text = objConv.rToF(inputValue).ToString()
+                        TextBox3.Text = objConv.rToF(inputValue).ToString()
+                    Case "Konversi Reamur - Kelvin"
+                        TextBox4.Text = objConv.rToK(inputValue).ToString()
+                        TextBox3.Text = objConv.rToK(inputValue).ToString()
+
+                    Case "Konversi Fahrenheit - Kelvin"
+                        TextBox4.Text = objConv.fToK(inputValue).ToString()
+                        TextBox3.Text = objConv.fToK(inputValue).ToString()
+
+                    Case Else
+                        ' Handle additional conversion cases if needed
+                End Select
+            End If
+        End If
+
+
+        If Double.TryParse(TextBox4.Text, inputValue) Then
+            TextBox3.Text = inputValue
+            If TextBox4.Enabled AndAlso Not TextBox4.ReadOnly Then
+                Select Case selectedItem
+                    Case "Konversi Celcius - Reamur"
+                        TextBox2.Text = objConv.rToc(inputValue).ToString()
+                        TextBox1.Text = objConv.rToc(inputValue).ToString()
+                    Case "Konversi Celcius - Fahrenheit"
+                        TextBox2.Text = objConv.fToC(inputValue).ToString()
+                        TextBox1.Text = objConv.fToC(inputValue).ToString()
+                    Case "Konversi Celcius - Kelvin"
+                        TextBox2.Text = objConv.kToc(inputValue).ToString()
+                        TextBox1.Text = objConv.kToc(inputValue).ToString()
+
+                    Case "Konversi Reamur - Fahrenheit"
+                        TextBox2.Text = objConv.fTor(inputValue).ToString()
+                        TextBox1.Text = objConv.fTor(inputValue).ToString()
+                    Case "Konversi Reamur - Kelvin"
+                        TextBox2.Text = objConv.kTor(inputValue).ToString()
+                        TextBox1.Text = objConv.kTor(inputValue).ToString()
+
+                    Case "Konversi Fahrenheit ke Kelvin"
+                        TextBox2.Text = objConv.kTof(inputValue).ToString()
+                        TextBox1.Text = objConv.kTof(inputValue).ToString()
+
+                    Case Else
+                        ' Handle additional conversion cases if needed
+                End Select
+            End If
+        End If
+
+    End Sub
+
+    Private Sub TextBox4_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox4.KeyPress
+        ' Allow only digits and control characters (like Backspace)
+        If Not Char.IsDigit(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        TextBox4.ReadOnly = True
+        TextBox4.Enabled = False
+        TextBox2.ReadOnly = False
+        TextBox2.Enabled = True
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        TextBox2.ReadOnly = True
+        TextBox2.Enabled = False
+        TextBox4.ReadOnly = False
+        TextBox4.Enabled = True
+    End Sub
+End Class
